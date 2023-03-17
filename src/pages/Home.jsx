0a10b0860219afd42b7031b7168288ede1bd5d4b
OdessaCh <odessa.chesneau@gmail.com>
2023-03-17 15:28:54 +0100

    WIP front home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,15 @@
 import Layout from "../components/Layout.jsx";
+import '../assets/css/home.css';
+
 import voiturehome from '../assets/cars/voiturehome.avif';
 import voitureblanche from '../assets/cars/voitureblanche.avif';
 import arrierevoiture from '../assets/cars/arrierevoiture.avif';
 import interieurvoiture from '../assets/cars/interieurvoiture.avif';
+import coffre from '../assets/cars/coffre.avif';
+import interieurvoiture2 from '../assets/cars/interieurvoiture2.avif';
+import portiere from '../assets/cars/portiere.avif';
+import roue from '../assets/cars/roue.avif';
+
 
 
 
@@ -11,10 +18,24 @@ const Home = () => {
         <Layout>
             <div className="home">
                 <h2>Homepage</h2>
-                <img src={voiturehome} class=""/>
                 <img src={voitureblanche} class=""/>
-                <img src={arrierevoiture} class=""/>
-                <img src={interieurvoiture} class=""/>
+                <p className="home-text"> Pour les trajets courts, privilégiez la marche ou le vélo. Au quotidien, prenez les transports en commun. Pensez à covoiturer. <br />#SeDéplacerMoinsPolluer
+                </p>
+                <img src={voiturehome} class=""/>
+
+                <img src={coffre} class=""/>
+                <img src={interieurvoiture2} class=""/>
+                <img src={portiere} class=""/>
+                <img src={roue} class=""/>
+
+
+                <div className="design">
+                    <img src={voitureblanche} class="img"/>
+                    <div className="design-car">
+                        <img src={interieurvoiture} class="img2"/>
+                        <img src={arrierevoiture} class="img2"/>
+                    </div>
+                </div>
             </div>
         </Layout>
     );
