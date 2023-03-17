00af220109ac1bc646e414690b722d924a27f831
OdessaCh <odessa.chesneau@gmail.com>
2023-03-17 11:13:56 +0100

    WIP add pictures on homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,20 @@
 import Layout from "../components/Layout.jsx";
+import voiturehome from '../assets/cars/voiturehome.avif';
+import voitureblanche from '../assets/cars/voitureblanche.avif';
+import arrierevoiture from '../assets/cars/arrierevoiture.avif';
+import interieurvoiture from '../assets/cars/interieurvoiture.avif';
+
+
 
 const Home = () => {
     return (
         <Layout>
             <div className="home">
                 <h2>Homepage</h2>
+                <img src={voiturehome} class=""/>
+                <img src={voitureblanche} class=""/>
+                <img src={arrierevoiture} class=""/>
+                <img src={interieurvoiture} class=""/>
             </div>
         </Layout>
     );
