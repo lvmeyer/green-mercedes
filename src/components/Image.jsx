e23f26cdfadf1137fee851759111c2d53f856e5f
OdessaCh <odessa.chesneau@gmail.com>
2023-04-03 21:38:07 +0200

    Template picture component

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -3,9 +3,9 @@ function Image({ src, alt }) {
     return (
             <>
                 <picture className="picture">
-                    <source media="(min-width: 650px)" alt={`${alt}_650`} srcset={`${src}`}/>
-                    <source media="(min-width: 430px)" alt={`${alt}_430`} srcset={`${src}`}/>
-                    <img src={`${src}`} alt={`${alt}_full`} style="width:auto;"/>
+                    <source media="(min-width: 650px)" alt={`${alt}_650`} srcset={`${src}_650.avif`}/>
+                    <source media="(min-width: 430px)" alt={`${alt}_430`} srcset={`${src}_430.avif`}/>
+                    <img src={`${src}_full.avif`} alt={`${alt}_full`} style="width:auto;"/>
                 </picture>
             </>
         );
