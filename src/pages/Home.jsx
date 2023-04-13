58e0d4fed22df23fc516282618300f1e27297e76
Mael JAMIN <mael.jamin@gmail.com>
2023-04-13 14:29:39 +0200

    scroll to elements

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,18 @@ import Layout from "../components/Layout.jsx";
 import intro_video from '../assets/videos/home/intro.mp4'
 import {lazy, Suspense} from "preact/compat";
 import '../assets/css/home.css';
-const Home = () => {
+import {Car, CarIcon, Mail, NewspaperIcon} from "lucide-preact";
+const Home = ({ scrollTo }) => {
     const CarsList = lazy(() => import('../components/CarsList.jsx'));
     const NewsList = lazy(() => import('../components/NewsList.jsx'));
+
+    if (scrollTo) {
+        const element = document.getElementById(scrollTo);
+        if (element) {
+            element.scrollIntoView({ behavior: 'smooth' });
+        }
+    }
+
     return (
         <Layout>
 
@@ -27,7 +36,7 @@ const Home = () => {
             </section>
 
             <section className="container" id="news">
-                <h3>Actualités</h3>
+                <h3 className="home-title"><NewspaperIcon size={40}/>Actualités</h3>
                 <Suspense fallback={<article aria-busy={true}></article>}>
                     <NewsList />
                 </Suspense>
@@ -36,7 +45,7 @@ const Home = () => {
             <hr />
 
             <section className="container" id="products">
-                <h3>Nos modèles</h3>
+                <h3 className="home-title"><CarIcon size={40}/> Nos modèles</h3>
                 <Suspense fallback={<article aria-busy={true}></article>}>
                     <CarsList />
                 </Suspense>
@@ -46,7 +55,7 @@ const Home = () => {
                 <div className="container">
                     <article>
                         <hgroup>
-                            <h2>NewsLetter</h2>
+                            <h2 className="home-title"> <Mail size={40} />NewsLetter</h2>
                             <h3>Abonnez-vous gratuitement et recevez les dernières actualités de Mercedes-Benz</h3>
                         </hgroup>
                         <form className="grid">
@@ -71,21 +80,6 @@ const Home = () => {
                     </article>
                 </div>
             </section>
-
-            <dialog>
-                <article>
-                    <header>
-                        <a href="#close" aria-label="Close" className="close"></a>
-                        Modal title
-                    </header>
-                    <p>
-                        Nunc nec ligula a tortor sollicitudin dictum in vel enim.
-                        Quisque facilisis turpis vel eros dictum aliquam et nec turpis.
-                        Sed eleifend a dui nec ullamcorper.
-                        Praesent vehicula lacus ac justo accumsan ullamcorper.
-                    </p>
-                </article>
-            </dialog>
 
         </Layout>
     );
